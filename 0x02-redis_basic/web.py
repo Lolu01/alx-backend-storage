56350668f37f9170b9ebd894119850e51203d8de
Lolu01 <jaiyeolalulu@gmail.com>
2024-10-24 12:21:37 +0000

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,29 +14,25 @@ redis_instance = redis.Redis()
 
 def cache_data(method: Callable) -> Callable:
     """
-    Decorator that counts the number of calls to a method using Redis
-    and caches the result for a specific period.
+    Decorator that counts the number of calls to a method using Redis.
     """
     @wraps(method)
     def inc(url: str) -> str:
         """
-        Increments the value of a Redis key and then calls the given method
-        Caches the result of the method for a certain period.
+        Increments the value of a Redis key and then calls the given method.
         """
         count_key = f"count:{url}"
-        result_key = f"result:{url}"
+        res_key = f"result:{url}"
         expiration_time = 10
         redis_instance.incr(count_key)
-        cached_content = redis_instance.get(result_key)
-        if cached_content:
-            print(f"Cache hit for {url}")
-            return cached_content.decode('utf-8')
-
-        print(f"Fetching new content for {url}")
-        content = method(url)
-        redis_instance.setex(result_key, expiration_time, content)
-        return content
-    return wrapper
+        res = redis_instance.get(res_key)
+        if res:
+            return res.decode('utf-8')
+        res = method(url)
+        redis_instance.set(count_key, 0)
+        redis_instance.setex(res_key, expiration_time, res)
+        return res
+    return inc
 
 
 @cache_data
@@ -46,10 +42,3 @@ def get_page(url: str) -> str:
     """
     response = requests.get(url)
     return response.text
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
-
-    print(get_page(url))
-
-    print(get_page(url))
