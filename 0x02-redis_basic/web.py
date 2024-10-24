c2e9fbc28598b34ad7a2fd4360bc1f7f13d564b7
Lolu01 <jaiyeolalulu@gmail.com>
2024-10-24 12:10:46 +0000

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -24,16 +24,19 @@ def cache_data(method: Callable) -> Callable:
         Caches the result of the method for a certain period.
         """
         count_key = f"count:{url}"
-        res_key = f"result:{url}"
+        result_key = f"result:{url}"
         expiration_time = 10
         redis_instance.incr(count_key)
-        res = redis_instance.get(res_key)
-        if res:
-            return res.decode('utf-8')
-        res = method(url)
-        redis_instance.setex(res_key, expiration_time, res)
-        return res
-    return inc
+        cached_content = redis_instance.get(result_key)
+        if cached_content:
+            print(f"Cache hit for {url}")
+            return cached_content.decode('utf-8')
+
+        print(f"Fetching new content for {url}")
+        content = method(url)
+        redis_instance.setex(result_key, expiration_time, content)
+        return content
+    return wrapper
 
 
 @cache_data
@@ -43,3 +46,10 @@ def get_page(url: str) -> str:
     """
     response = requests.get(url)
     return response.text
+
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
+
+    print(get_page(url))
+
+    print(get_page(url))
